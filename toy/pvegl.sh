a3acf67846533aaad47eef7a7b4ac0d3a64a460b
starry <csywasde@outlook.com>
2026-02-12 10:49:46 +0800

    Update pvegl.sh

--- toy/pvegl.sh
+++ toy/pvegl.sh
@@ -7,13 +7,13 @@ RESET="\033[0m"
 menu() {
     clear
     echo -e "${GREEN}=== PVE 管理菜单 ===${RESET}"
-    echo -e "${GREEN}1) 添加 SWAP${RESET}"
+    echo -e "${GREEN}1) 添加SWAP${RESET}"
     echo -e "${GREEN}2) 检测环境${RESET}"
-    echo -e "${GREEN}3) PVE 主体安装${RESET}"
+    echo -e "${GREEN}3) PVE主体安装${RESET}"
     echo -e "${GREEN}4) 预配置环境${RESET}"
     echo -e "${GREEN}5) 自动配置宿主机网关${RESET}"
-    echo -e "${GREEN}6) 开设 KVM 小鸡${RESET}"
-    echo -e "${GREEN}7) 开设 LXC 小鸡${RESET}"
+    echo -e "${GREEN}6) 开设KVM小鸡${RESET}"
+    echo -e "${GREEN}7) 开设LXC小鸡${RESET}"
     echo -e "${GREEN}0) 退出${RESET}"
     read -p $'\033[32m请选择操作: \033[0m' choice
     case $choice in
