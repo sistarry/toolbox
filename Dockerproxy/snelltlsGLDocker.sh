bd8eba25c54ae07174d311fd9187c1f4303df08e
starry <csywasde@outlook.com>
2026-03-05 20:30:37 +0800

    Update snelltlsGLDocker.sh

--- Dockerproxy/snelltlsGLDocker.sh
+++ Dockerproxy/snelltlsGLDocker.sh
@@ -74,7 +74,7 @@ select_node() {
 
 install_node() {
     check_docker
-    read -p "请输入节点名称 [node$(date +%s)]: " NODE_NAME
+    read -p "请输入节点名称 [默认node$(date +%s)]: " NODE_NAME
     NODE_NAME=${NODE_NAME:-node$(date +%s)}
     NODE_DIR="$APP_DIR/$NODE_NAME"
     mkdir -p "$NODE_DIR/snell-conf"
