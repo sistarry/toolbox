de102cf2270c3c99738b1e62126341a56fe064c9
starry <csywasde@outlook.com>
2026-04-17 18:45:43 +0800

    Update SSLbackup.sh

--- VPS/SSLbackup.sh
+++ VPS/SSLbackup.sh
@@ -6,6 +6,7 @@ RESET="\033[0m"
 
 NGINX_URL="https://raw.githubusercontent.com/sistarry/toolbox/main/VPS/nginxbackup.sh"
 CAADY_URL="https://raw.githubusercontent.com/sistarry/toolbox/main/VPS/caadybackup.sh"
+ACME_URL="https://raw.githubusercontent.com/sistarry/toolbox/main/VPS/acmebackup.sh"
 
 run_script() {
     url=$1
@@ -27,11 +28,15 @@ menu() {
     echo -e "${GREEN}============================${RESET}"
     echo -e "${GREEN}1) Nginx 证书备份与恢复${RESET}"
     echo -e "${GREEN}2) Caddy 证书备份与恢复${RESET}"
+    echo -e "${GREEN}3) ACME 证书备份与恢复${RESET}"
     echo -e "${GREEN}0) 退出${RESET}"
+
     read -p $'\033[32m请选择: \033[0m' choice
+
     case $choice in
         1) run_script "$NGINX_URL" "Nginx 证书备份与恢复" ;;
-        2) run_script "$CAADY_URL" "Caady 证书备份与恢复" ;;
+        2) run_script "$CAADY_URL" "Caddy 证书备份与恢复" ;;
+        3) run_script "$ACME_URL" "ACME 证书备份与恢复" ;;
         0) exit 0 ;;
         *)
             echo -e "${RED}输入错误，请重新选择${RESET}"
