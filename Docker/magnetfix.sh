e857a0f7a4310675399c9b0904839730408c424a
starry <csywasde@outlook.com>
2026-04-17 21:14:08 +0800

    Update magnetfix.sh

--- Docker/magnetfix.sh
+++ Docker/magnetfix.sh
@@ -43,7 +43,7 @@ get_public_ip() {
 menu() {
     while true; do
         clear
-        echo -e "${GREEN}=== magnet_fix 管理菜单 ===${RESET}"
+        echo -e "${GREEN}=== magnetfix 管理菜单 ===${RESET}"
         echo -e "${GREEN}1) 安装启动${RESET}"
         echo -e "${GREEN}2) 更新${RESET}"
         echo -e "${GREEN}3) 重启${RESET}"
