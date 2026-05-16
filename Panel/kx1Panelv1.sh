f1ddd0be19f29c867e9fa5de69fb66de445143d0
starry <csywasde@outlook.com>
2026-05-16 09:34:21 +0800

    Update kx1Panelv1.sh

--- Panel/kx1Panelv1.sh
+++ Panel/kx1Panelv1.sh
@@ -12,7 +12,7 @@ menu() {
     echo -e "${GREEN}1) 安装${RESET}"
     echo -e "${GREEN}2) 更新${RESET}"
     echo -e "${GREEN}3) 管理${RESET}"
-    echo -e "${GREEN}3) 卸载${RESET}"
+    echo -e "${GREEN}4) 卸载${RESET}"
     echo -e "${GREEN}0) 退出${RESET}"
     read -p $'\033[32m请选择操作: \033[0m' choice
     case $choice in
