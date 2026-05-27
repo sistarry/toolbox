1d7951586cc673c8f0eaf588bf8be5ad33b26ad3
starry <csywasde@outlook.com>
2026-05-27 21:26:20 +0800

    Update bbros.sh

--- PROXY/bbros.sh
+++ PROXY/bbros.sh
@@ -83,10 +83,10 @@ case "$OS" in
         ;;
     debian|ubuntu|centos|rocky|almalinux|fedora)
         echo -e "${GREEN}检测到系统为 $OS (KVM/独立内核)，调用标准 BBR${RESET}"
-        bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/PROXY/BBRTCP.sh)
+        bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/PROXY/BBRTU.sh)
         ;;
     *)
         echo -e "${YELLOW}未能精准识别系统，尝试运行通用BBR...${RESET}"
-        bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/PROXY/BBRTCP.sh)
+        bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/PROXY/BBRTU.sh)
         ;;
 esac
