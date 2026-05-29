63f89d15cbb3bc02957096ece0b3bf6bc130a2c8
starry <csywasde@outlook.com>
2026-05-29 19:52:21 +0800

    Update Alpine.sh

--- Alpine/Alpine.sh
+++ Alpine/Alpine.sh
@@ -54,7 +54,7 @@ menu() {
     echo -e "${YELLOW}[12] 修改主机名${RESET}"
     echo -e "${YELLOW}[13] DNS设置${RESET}"
     echo -e "${YELLOW}[14] Docker管理${RESET}"
-    echo -e "${YELLOW}[15] 应用商店${RESET}"
+    echo -e "${YELLOW}[15] 反向代理${RESET}"
     echo -e "${YELLOW}[16] Shadowsocks${RESET}"
     echo -e "${YELLOW}[17] Reality${RESET}"
     echo -e "${YELLOW}[18] Hysteria2${RESET}"
@@ -71,31 +71,31 @@ menu() {
     echo -ne "${RED}请输入操作编号: ${RESET}"
     read choice
     case "$choice" in
-        1) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/apup.sh) ;;
-        2) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/apx.sh) ;;
-        3) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/apql.sh) ;;
+        1) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/VPS/update.sh) ;;
+        2) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/VPS/vpsinfo.sh) ;;
+        3) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/APClear.sh) ;;
         4) sudo reboot ;;
-        5) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/apsdk.sh) ;;
+        5) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/APSSHDK.sh) ;;
         6) sudo passwd root ;;
-        7) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/apssh.sh) ;;
-        8) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/apfeew.sh) ;;
-        9) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/apFail2Ban.sh) ;;
-        10) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/aphuanyuan.sh) ;;
-        11) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/apcn.sh) ;;
-        12) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/aphome.sh) ;;
-        13) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/apdns.sh) ;;
-        14) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/apdocker.sh) ;;
-        15) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Docker/Store.sh) ;;
-        16) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/AZAPSS.sh) ;;
-        17) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/AZAPReality.sh) ;;
-        18) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/AZAPHysteria2.sh) ;;
-        19) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/PROXY/Xray2go.sh) ;;
+        7) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/APSSHKey.sh) ;;
+        8) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/APfirewall.sh) ;;
+        9) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/APFail2Ban.sh) ;;
+        10) bash <(curl -sSL https://linuxmirrors.cn/main.sh) ;;
+        11) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/APZT.sh) ;;
+        12) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/VPS/home.sh) ;;
+        13) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/APDNS.sh) ;;
+        14) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/APDocker.sh) ;;
+        15) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/APFD.sh) ;;
+        16) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/APSS.sh) ;;
+        17) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/APReality.sh) ;;
+        18) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/APhy2.sh) ;;
+        19) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/PROXY/2go.sh) ;;
         20) bash <(wget -qO- https://raw.githubusercontent.com/fscarmen/sing-box/main/sing-box.sh) ;;
         21) wget -O vless-server.sh https://raw.githubusercontent.com/Zyx0rx/vless-all-in-one/main/vless-server.sh && chmod +x vless-server.sh && ./vless-server.sh ;;
-        22) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/3xuiAlpine.sh) ;;
+        22) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/AP3X-UI.sh) ;;
         23) wget -qO- https://raw.githubusercontent.com/zywe03/realm-xwPF/main/xwPF.sh | sudo bash -s install ;;
-        24) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/EmbyAlpine.sh) ;;
-        25) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/apagent.sh) ;;
+        24) curl -fsSL -o install.sh https://raw.githubusercontent.com/admintors/emby-proxy-alpine-lite/main/install.sh && bash install.sh ;;
+        25) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/toy/unagent.sh) ;;
         88)
             echo -e "${YELLOW}🔄 正在更新脚本...${RESET}"
             curl -fsSL -o "$SCRIPT_PATH" "$SCRIPT_URL" || {
