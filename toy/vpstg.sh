aac84e223a7515295bcef6d16f35e593050873f5
starry <csywasde@outlook.com>
2026-06-05 22:11:36 +0800

    Update vpstg.sh

--- toy/vpstg.sh
+++ toy/vpstg.sh
@@ -14,17 +14,17 @@ menu() {
     read -p $'\033[32m请选择操作: \033[0m' choice
     case $choice in
         1)
-            echo -e "${GREEN}正在运行系统信息脚本...${RESET}"
+            echo -e "${GREEN}正在运行系统信息...${RESET}"
             bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/toy/vpsxin.sh)
             pause
             ;;
         2)
-            echo -e "${GREEN}正在运行网卡信息脚本...${RESET}"
+            echo -e "${GREEN}正在运行网卡信息...${RESET}"
             bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/toy/network.sh)
             pause
             ;;
         3)
-            echo -e "${GREEN}正在运行Docker信息脚本...${RESET}"
+            echo -e "${GREEN}正在运行Docker信息...${RESET}"
             bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/toy/vpsd.sh)
             pause
             ;;
