cf432a9a5c501eea8924e00f6f1969dc19ec40ac
starry <csywasde@outlook.com>
2026-06-06 08:54:55 +0800

    Update vnstattgos.sh

--- OS/vnstattgos.sh
+++ OS/vnstattgos.sh
@@ -37,10 +37,10 @@ case "$OS" in
         ;;
     debian|ubuntu|centos|rocky|almalinux|fedora)
         # 执行原版脚本
-        fetch_and_run "https://raw.githubusercontent.com/sistarry/toolbox/main/VPS/vnstattg.sh"
+        fetch_and_run "https://raw.githubusercontent.com/sistarry/toolbox/main/toy/vnstattg.sh"
         ;;
     *)  
         # 未能识别或暂不支持您的系统
-        fetch_and_run "https://raw.githubusercontent.com/sistarry/toolbox/main/VPS/vnstattg.sh"
+        fetch_and_run "https://raw.githubusercontent.com/sistarry/toolbox/main/toy/vnstattg.sh"
         ;;
 esac
