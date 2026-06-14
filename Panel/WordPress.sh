620bd21216b92394a5b3551167247a8a91981e4a
starry <csywasde@outlook.com>
2026-06-14 12:52:16 +0800

    Update WordPress.sh

--- Panel/WordPress.sh
+++ Panel/WordPress.sh
@@ -49,7 +49,7 @@ get_public_ip() {
 menu() {
     while true; do
         clear
-        echo -e "${GREEN}=== WordPress 管理菜单 ===${RESET}"
+        echo -e "${GREEN}=== WordPress(远程数据)管理菜单 ===${RESET}"
         echo -e "${GREEN}1. 安装启动${RESET}"
         echo -e "${GREEN}2. 更新${RESET}"
         echo -e "${GREEN}3. 重启${RESET}"
