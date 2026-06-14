7832e7cbf51fe7b4bb9b195332da2c43ea3e3458
starry <csywasde@outlook.com>
2026-06-14 13:00:09 +0800

    Update lskyprodb.sh

--- Docker/lskyprodb.sh
+++ Docker/lskyprodb.sh
@@ -13,7 +13,7 @@ CONFIG_FILE="$APP_DIR/config.env"
 
 function menu() {
     clear
-    echo -e "${GREEN}=== Lsky-Pro 管理菜单 ===${RESET}"
+    echo -e "${GREEN}=== Lsky-Pro+MySQL管理菜单 ===${RESET}"
     echo -e "${GREEN}1) 安装启动${RESET}"
     echo -e "${GREEN}2) 更新${RESET}"
     echo -e "${GREEN}3) 卸载(含数据)${RESET}"
