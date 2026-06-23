504cf023261fc35d1ab3d46b56d6f5bd924b1b67
starry <csywasde@outlook.com>
2026-06-23 12:08:53 +0800

    Update LXDAPI.sh

--- NAT/LXDAPI.sh
+++ NAT/LXDAPI.sh
@@ -5,6 +5,7 @@ RED="\033[31m"
 RESET="\033[0m"
 
 LXD_INSTALL="https://raw.githubusercontent.com/xkatld/lxdapi-web-server/refs/heads/main-stable/Shell/lxd_install.sh"
+ZFS_INSTALL="https://raw.githubusercontent.com/xkatld/lxdapi-web-server/main-stable/Shell/lxdapi_zfsinstall.sh"
 API_INSTALL="https://raw.githubusercontent.com/xkatld/lxdapi-web-server/refs/heads/main-stable/Shell/lxdapi_install.sh"
 API_UPDATE="https://raw.githubusercontent.com/xkatld/lxdapi-web-server/refs/heads/main-stable/Shell/lxdapi_update.sh"
 STORAGE_POOL="https://raw.githubusercontent.com/xkatld/lxdapi-web-server/refs/heads/main-stable/Shell/storage_pool.sh"
@@ -37,26 +38,28 @@ menu() {
     echo -e "${GREEN}    ◈   LXDAPI 管理工具   ◈    ${RESET}"
     echo -e "${GREEN}===============================${RESET}"
     echo -e "${GREEN} 1) 安装 LXD${RESET}"
-    echo -e "${GREEN} 2) 部署 LXDAPI${RESET}"
-    echo -e "${GREEN} 3) 更新 LXDAPI${RESET}"
-    echo -e "${GREEN} 4) 管理存储池${RESET}"
-    echo -e "${GREEN} 5) 管理镜像${RESET}"
-    echo -e "${GREEN} 6) 新增镜像${RESET}"
-    echo -e "${GREEN} 7) 查看公网网卡${RESET}"
-    echo -e "${GREEN} 8) LXDAPI面板信息${RESET}"
+    echo -e "${GREEN} 2) 安装 ZFS (Ubuntu 跳过)${RESET}"
+    echo -e "${GREEN} 3) 部署 LXDAPI${RESET}"
+    echo -e "${GREEN} 4) 更新 LXDAPI${RESET}"
+    echo -e "${GREEN} 5) 管理存储池${RESET}"
+    echo -e "${GREEN} 6) 管理镜像${RESET}"
+    echo -e "${GREEN} 7) 新增镜像${RESET}"
+    echo -e "${GREEN} 8) 查看公网网卡${RESET}"
+    echo -e "${GREEN} 9) LXDAPI面板信息${RESET}"
     echo -e "${GREEN} 0) 退出${RESET}"
     echo -e "${GREEN}===============================${RESET}"
     read -p $'\033[32m 请选择: \033[0m' choice
 
     case $choice in
         1) run "$LXD_INSTALL" "安装 LXD" ;;
-        2) run "$API_INSTALL" "部署 LXDAPI" ;;
-        3) run "$API_UPDATE" "更新 LXDAPI" ;;
-        4) run "$STORAGE_POOL" "存储池管理" ;;
-        5) run "$IMAGE_IMPORT" "镜像管理" ;;
-        6) run "$IMAGE_ADD" "新增镜像" ;;
-        7) show_ip ;;
-        8) run "$IMAGE_XX" "面板信息" ;;
+        2) run "$ZFS_INSTALL" "安装 ZFS" ;;
+        3) run "$API_INSTALL" "部署 LXDAPI" ;;
+        4) run "$API_UPDATE" "更新 LXDAPI" ;;
+        5) run "$STORAGE_POOL" "存储池管理" ;;
+        6) run "$IMAGE_IMPORT" "镜像管理" ;;
+        7) run "$IMAGE_ADD" "新增镜像" ;;
+        8) show_ip ;;
+        9) run "$IMAGE_XX" "面板信息" ;;
         0) exit 0 ;;
         *)
             echo -e "${RED}输入错误，请重新选择${RESET}"
