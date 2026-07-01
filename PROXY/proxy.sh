fadd762f81752fd95a48852de7cb156fb7dabfc9
starry <csywasde@outlook.com>
2026-07-01 09:22:51 +0800

    Update proxy.sh

--- PROXY/proxy.sh
+++ PROXY/proxy.sh
@@ -12,7 +12,7 @@ RESET="\033[0m"
 BOLD="\033[1m"
 ORANGE='\033[38;5;208m'
 
-SCRIPT_PATH="/root/proxy.sh"
+SCRIPT_PATH="/etc/proxy.sh"
 SCRIPT_URL="https://raw.githubusercontent.com/sistarry/toolbox/main/PROXY/proxy.sh"
 BIN_LINK_DIR="/usr/local/bin"
 
